c1c7ea0e1df036e30e16f6f254840d40723634fb
Francisco Veloz <fveloz@me.com>
2013-06-10 16:24:55 -0400

    ability to remove fields

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -3,8 +3,8 @@ class Brew < ActiveRecord::Base
 	has_many :extras
 	has_many :hops
 	has_many :yeasts
-	accepts_nested_attributes_for :hops
-	accepts_nested_attributes_for :fermentables
-	accepts_nested_attributes_for :extras
-	accepts_nested_attributes_for :yeasts
+	accepts_nested_attributes_for :hops, allow_destroy: true
+	accepts_nested_attributes_for :fermentables, allow_destroy: true
+	accepts_nested_attributes_for :extras, allow_destroy: true
+	accepts_nested_attributes_for :yeasts, allow_destroy: true
 end
